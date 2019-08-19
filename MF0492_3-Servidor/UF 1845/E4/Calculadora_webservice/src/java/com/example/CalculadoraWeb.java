c0f9ff440f938f050767ae4d304f789104e6be7e
José Sánchez <sanchezjoja@gmail.com>
2019-08-19 13:22:17 +0100

    19/08/2019

---
+++ MF0492_3-Servidor/UF 1845/E4/Calculadora_webservice/src/java/com/example/CalculadoraWeb.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Alumno
+ */
+@WebService(serviceName = "CalculadoraWeb")
+public class CalculadoraWeb {
+
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "Addition")
+    public String Addition(@WebParam(name = "i") float i, @WebParam(name = "j") float j) {
+        float res = i + j;   
+        return (Float.toString(res));
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "Subtraction")
+    public String Subtraction(@WebParam(name = "i") float i, @WebParam(name = "j") float j) {
+       float res = i - j;   
+        return (Float.toString(res));
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "Multiplication")
+    public String Multiplication(@WebParam(name = "i") float i, @WebParam(name = "j") float j) {
+        float res = i * j;   
+        return (Float.toString(res));
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "Division")
+    public String Division(@WebParam(name = "i") float i, @WebParam(name = "j") float j) {
+       float res = i / j;   
+        return (Float.toString(res));
+    }
+}
